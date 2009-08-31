57c4ba1e95c99e4fb880d59492892cb23c6f58db
Chris Lewis <chris@chris.to>
2009-08-31 16:32:43 -0700

    Refactored packages, added a rule to check if Mario is ducking

---
+++ src/main/java/edu/ucsc/eis/mario/KnowledgeReader.java
@@ -0,0 +1,33 @@
+package edu.ucsc.eis.mario;
+
+import org.drools.KnowledgeBase;
+import org.drools.KnowledgeBaseFactory;
+import org.drools.builder.KnowledgeBuilder;
+import org.drools.builder.KnowledgeBuilderError;
+import org.drools.builder.KnowledgeBuilderErrors;
+import org.drools.builder.KnowledgeBuilderFactory;
+import org.drools.builder.ResourceType;
+import org.drools.io.ResourceFactory;
+
+import com.google.common.base.Preconditions;
+
+public class KnowledgeReader {
+	public static KnowledgeBase getKnowledgeBase(String ruleResource) 
+		throws IllegalArgumentException {
+		Preconditions.checkNotNull(ruleResource, "Must pass a resource");
+		KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
+		kbuilder.add(ResourceFactory.newClassPathResource(ruleResource), ResourceType.DRL);
+		KnowledgeBuilderErrors errors = kbuilder.getErrors();
+		
+		if (errors.size() > 0) {
+			for (KnowledgeBuilderError error: errors) {
+				System.err.println(error);
+			}
+			throw new IllegalArgumentException("Could not parse knowledge.");
+		}
+		
+		KnowledgeBase kbase = KnowledgeBaseFactory.newKnowledgeBase();
+		kbase.addKnowledgePackages(kbuilder.getKnowledgePackages());
+		return kbase;
+	}
+}
